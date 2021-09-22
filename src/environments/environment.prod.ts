e1ae7bd80b0f2c0b01860fa74768f8e771b27192
Daniel Cruz Chavarría <danielcruzch8@gmail.com>
2021-09-21 21:20:06 -0500

    add enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2-WQuiyz25DeRSFakSYyPGHXwRQNp8p8",
+    authDomain: "crud-bl-daniel-c.firebaseapp.com",
+    projectId: "crud-bl-daniel-c",
+    storageBucket: "crud-bl-daniel-c.appspot.com",
+    messagingSenderId: "937744222974",
+    appId: "1:937744222974:web:d95474001f0a52eff1f3af",
+    measurementId: "G-TJWML09FGR"
+  }
 };
